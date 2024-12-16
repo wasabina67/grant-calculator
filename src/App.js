972fef2a4fd97fb7f78a6b60354d03b6019b07c0
wasabina67 <wasabina67@gmail.com>
2024-12-16 11:53:15 +0900

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ function App() {
   return (
     <div style={{ maxWidth: "400px", margin: "0 auto", fontFamily: "Arial, sans-serif" }}>
       <h1>grant-calculator</h1>
-      <ul>
-        <li>America</li>
-        <li>Brazil</li>
-        <li>Canada</li>
-        <li>Denmark</li>
+      <ul style={{ padding: "0px 0px 0px 20px" }}>
+        <li>補助上限</li>
+        <ul>
+          <li>aaaaaaaaaaaa</li>
+        </ul>
+        <li>補助率</li>
+        <ul>
+          <li>aaaaaaaaaaaa</li>
+        </ul>
       </ul>
+      <div>補助上限</div>
+      <div>補助率</div>
     </div>
   );
 }
