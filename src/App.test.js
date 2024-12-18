cec42c37de97eb1f593a6388de28d593397bdae7
wasabina67 <wasabina67@gmail.com>
2024-12-18 11:59:33 +0900

    Update src/App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,22 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-const runTest = ({limit, rate, result}) => {
+const runTest = ({limit, rate, expectedResult}) => {
   test('first', () => {
     render(<App />);
 
     const grantLimitInput = screen.getByPlaceholderText('200');
-    fireEvent.change(grantLimitInput, { target: { value: '200' } });
+    fireEvent.change(grantLimitInput, { target: { value: limit } });
 
     const grantRateInput = screen.getByPlaceholderText('0.75');
-    fireEvent.change(grantRateInput, { target: { value: '0.75' } });
+    fireEvent.change(grantRateInput, { target: { value: rate } });
 
     const calcButton = screen.getByText('計算');
     fireEvent.click(calcButton);
 
-    const result = screen.getByText('266.67');
+    const result = screen.getByText(expectedResult);
     expect(result).toBeInTheDocument();
   });
 };
 
-runTest('', '', '');
+runTest({ limit: '200', rate: '0.75', expectedResult: '266.67' });
