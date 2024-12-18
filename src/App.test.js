fafe2d2c63e331b790a1a369ae903d29e400b30c
wasabina67 <wasabina67@gmail.com>
2024-12-18 11:19:52 +0900

    Update src/App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('first', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const grantLimitInput = screen.getByPlaceholderText('200');
+  fireEvent.change(grantLimitInput, { target: { value: '200' } });
+
+  const grantRateInput = screen.getByPlaceholderText('0.75');
+  fireEvent.change(grantRateInput, { target: { value: '0.75' } });
+
+  const calcButton = screen.getByText('計算');
+  fireEvent.click(calcButton);
+
+  const result = screen.getByText('266.67');
+  expect(result).toBeInTheDocument();
 });
